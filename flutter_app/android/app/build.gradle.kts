37ff7eb92e08c60db1e0e1d48a71b2b050335ae9
Eddie Chin Jia Ze <1211101093@student.mmu.edu.my>
2026-04-06 05:00:19 +0000

    Fix Android package id mismatch causing app launch crash

--- flutter_app/android/app/build.gradle.kts
+++ flutter_app/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.phishguard.phish_guard"
+    namespace = "com.phishguard.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -20,8 +20,8 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.phishguard.phish_guard"
+        // Keep app id aligned with MainActivity package for stable Android launches.
+        applicationId = "com.phishguard.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
